f3df68c5f1c53830e92d1b779d1f5fc3f3a1967b
MarcoH03 <marcocanizarestan0331@gmail.com>
2024-06-29 02:29:47 -0400

    Version Base del codigo del profe [ ] Plotear funciones de : 	—Obligado: 	[ ] 2 variables no animado 	[ ] 3 variables animadas
    	—Opcional:
    	[ ] Grafico de funciones vectoriales still y animated
    
    [ ] Agregar los comandos:
    	[ ] - -kind : (plot, scatter, polar, contour, plot 3d, plot surface)
    	[ ] - - animation: (true, false solo para funciones de 2 variables)
    	[ ] - - core: (cuantos)
    	[ ] — range: (rango de cada variable )
    	[ ] /config : (cambia los valores default)

--- main.py
+++ main.py
@@ -1,60 +1,63 @@
 
-import multiprocessing
 from tools import handle_define, handle_evaluate, handle_gen
-import matplotlib.pyplot as plt
-import numpy as np
-from multiprocessing import Pool
+
+import matplotlib
+
+matplotlib.use("TkAgg")
 
 
-#primero: Identifico la operacion que la linea del texto debe realizar
-def process_line(line):
-    if line.startswith("/"):
-        tokens = line[1:].split(" ")
-        handle_buildin_function(tokens)
-        print("voy a ejecutar uno de los comandos reservados")
-
-    elif "=" in line:
-        handle_define(line)
-    else:
-        return handle_evaluate(line)
-    
-#segundo: Reviso si la operacion es una funcion reservada y de ser asi la ejecuto
-def handle_buildin_function(tokens):
-    buildin_function = tokens[0]
-    assert buildin_function in buildin_functions, "no conozco esta funcion"
-    fun = buildin_functions[buildin_function]
-    result = fun(*tokens[1:])
-    return result
-
-#tercero: Si el comando es load entonces defino una funcion nueva o evaluo una ya definida en un punto especifico
 def handle_load(path, *args):
     print(path)
     with open(file=path) as file:
-        for ix, line in enumerate(file.readlines()):
+        lines = file.readlines()
+        for ix, line in enumerate(lines):
+            print(f'line{ix}:{line}')
             if "=" in line:
                 handle_define(line)
             else:
                 result = handle_evaluate(line)
                 print(f'line{ix}:{result}')
 
-#Funciones reservadas
-buildin_functions = {
+
+build_in_functions = {
     "gen": handle_gen,
     "load": handle_load
 }
 
 
+def handle_build_in_function(tokens):
+    tokens = [*tokens]
+    build_in_function = tokens.pop(0)
+    assert build_in_function in build_in_functions, "no conozco esta funcion"
+    func = build_in_functions[build_in_function]
+    result = func(*tokens)
+    return result
+
+
+def process_line(line):
+    if line.startswith("/"):
+        tokens = line[1:].split(" ")
+        handle_build_in_function(tokens)
+        print("voy a ejecutar uno de los comandos reservados")
+
+    elif "=" in line:
+        handle_define(line)
+    else:
+        return handle_evaluate(line)
+
+
 if __name__ == "__main__":
     lines = [
         "/load input.txt",
-        "/gen f",
-        "/gen h",
-        "/gen g"
-        
+        # "f(6)",
+        "/gen f --kind=plane",
+        "/gen j",
+        # 'sin(0)'
+
     ]
     for line in lines:
         try:
-            process_line(line)
+            print(process_line(line))
         except AssertionError as err:
             print(err)
 
--- tools.py
+++ tools.py
@@ -1,204 +1,16 @@
+from itertools import product
 import pickle
 from multiprocessing import Pool
-from multiprocessing import cpu_count
 import re
-import operator
-import math
-from collections import deque
+
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
-from tqdm import tqdm
 
-# Diccionario de operadores con su precedencia, asociatividad y funcion correspondiente
-OPERATORS = {
-    '+': (1, 'L', operator.add),
-    '-': (1, 'L', operator.sub),
-    '*': (2, 'L', operator.mul),
-    '/': (2, 'L', operator.truediv),
-    '^': (3, 'R', operator.pow),
-    'neg': (4, 'R', operator.neg)
-}
+from const import *
+from rpn_notation import evaluate_function, evaluate_postfix, shunting_yard
 
-# Diccionario de funciones matematicas
-FUNCTIONS = {
-    'sin': math.sin,
-    'cos': math.cos,
-    'tan': math.tan,
-    'cot': lambda x: 1 / math.tan(x),
-    'log': math.log,
-    'log10': math.log10,
-    'sqrt': math.sqrt
-}
 
-# Diccionario de constantes matematicas
-CONSTANTS = {
-    'pi': math.pi,
-    'e': math.e
-}
-
-#funciones definidas por el usuario
-func_defs = {}
-
-#primero: Genera los puntos a plotear y distribulle la tarea de calcularlos en varios procesos
-def handle_gen(func, *args):
-    save_database()
-
-    (args, _) = func_defs[func]
-    params = []
-    for _ in range(len(args)):
-        params.append(
-            list(
-                np.arange(
-                    _rango[0],
-                    _rango[1],
-                    _rango[2]
-                )
-            )
-        )
-
-    if len(args) == 1:
-        params_1 = params[0]
-
-        result = []
-        with Pool(int(cpu_count())-1) as p:
-            result = p.map(
-                _handle_gen,
-                tqdm([
-                    (func, param_1)
-                    for param_1 in params_1
-                ])
-            )
-        result = list(zip(params_1, result))
-
-        fig, ax = plt.subplots()
-        np_result = np.array(result)
-        # Plot some data on the Axes.
-        ax.plot(np_result[:, 0], np_result[:, 1])
-        plt.show()
-    else:
-        # all combinations for functions with 2++ variables
-        grids = np.meshgrid(*params)
-        param_combinations = np.vstack(list(map(np.ravel, grids))).T
-
-        with Pool(int(cpu_count())-1) as p:
-            result = p.map(_handle_gen, tqdm([(func, *param) for param in param_combinations]))
-            
-        if len(args) == 2:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            xs, ys = grids
-            zs = np.array(result).flatten()
-            ax.plot3D(xs.ravel(), ys.ravel(), zs)
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-            plt.show()
-            
-        if len(args) == 3:
-            third_param_values = params[2]
-            frames_data = []
-
-            # Data para cada frame
-            for third_param_value in tqdm(third_param_values, desc="Preparing frames"):
-                current_combinations = [(func, *param) for param in param_combinations if param[2] == third_param_value]
-                
-                with Pool(int(cpu_count())-1) as p:
-                    result = p.map(_handle_gen, current_combinations)
-                
-                frames_data.append(result)
-            
-            # Plotting setup
-            fig, ax = plt.subplots()
-            xs, ys = np.array([param[:2] for param in param_combinations if param[2] == third_param_values[0]]).T
-            scat = ax.scatter(xs, ys, c=frames_data[0])  # Initial frame
-
-            def update(frame_number):
-                scat.set_array(frames_data[frame_number])
-                return scat,
-
-            ani = FuncAnimation(fig, update, frames=len(third_param_values), blit=True)
-            
-            # Save the animation as a video file
-            ani.save('function_animation.mp4', writer='ffmpeg', fps=5)
-
-            plt.show()
-
-#segundo: Actualiza la base de datos de funciones definidas por el usuario
-def save_database():
-    with open(f'.save_functions.txt', mode='wb') as f:
-        pickle.dump(func_defs, f)
-
-#segundo: Carga la base de datos de funciones definidas por el usuario
-def load_database():
-    global func_defs
-    with open(f'.save_functions.txt', mode='rb') as f:
-        func_defs = pickle.load(f)
-
-#tercero: recibe los parametros de la funcion en un punto y los evalua 
-def _handle_gen(args):
-    func_name = args[0]
-    args = [*args[1:]]
-    #print(f"Generando grafico de {func_name} con parametros {args}")
-    load_database()
-
-    func_desciption = func_defs[func_name]
-
-    return evaluate_function(func_desciption, args)
-
-#cuarto: Ordena lo que recive de _handle_gen y lo manda a evaluar
-def evaluate_function(func, args):
-    params, body = func
-    if len(params) != len(args):
-        raise ValueError("Numero incorrecto de argumentos")
-    return evaluate_expression(body, dict(zip(params, args)))
-
-#quinto: Boberia 
-def evaluate_expression(expression, vars):
-
-    return evaluate_postfix(expression, vars)
-
-#sexto: Evalua la funcion en el punto y devuelve el numero
-def evaluate_postfix(postfix, vars):
-    stack = []
-    while postfix:
-        token = postfix.popleft()
-        if isinstance(token, float):
-            stack.append(token)
-        elif isinstance(token, tuple):
-            func_name, arg_count = token
-            args = [stack.pop() for _ in range(arg_count)]
-            if func_name in func_defs:
-                stack.append(evaluate_function(
-                    func_defs[func_name], args[::-1]))
-            else:
-                stack.append(FUNCTIONS[func_name](*args[::-1]))
-        elif token in vars:
-            stack.append(vars[token])
-        elif token in CONSTANTS:
-            stack.append(CONSTANTS[token])
-        elif token in OPERATORS:
-            if len(stack) < 2 and token != 'neg':
-                raise ValueError(
-                    f"Error: no hay suficientes operandos para el operador '{token}'")
-            b = stack.pop()
-            a = stack.pop() if token != 'neg' else 0
-            stack.append(OPERATORS[token][2](a, b))
-        else:
-            raise ValueError(f"Token desconocido: {token}")
-
-    return stack[0]
-
-#2 primero: Primer paso para definir una nueva funcion. Manda la funcion a decomponer en trozos trabajables
-def handle_define(func_def):
-    try:
-        func_name, func_params, func_body = parse_function_definition(func_def)
-        func_defs[func_name] = (func_params, func_body)
-        return f"Funcion '{func_name}' definida exitosamente."
-    except ValueError as e:
-        return f"Error: {e}"
-
-#2 segundo: Descompone la funcion en trozos trabajables, identifica lo elemento por lo que son y los manda al Shyard
 def parse_function_definition(func_def):
     match = re.match(
         r'^\s*([a-zA-Z_]\w*)\s*\(([^)]*)\)\s*=\s*(.+)\s*$', func_def)
@@ -211,98 +23,90 @@ def parse_function_definition(func_def):
     idents = {text for t_name, text in tokens if t_name == 'ident'}
 
     undefined = idents - set(params) - set(OPERATORS) - \
-        set(FUNCTIONS) - set(CONSTANTS)
+        set(BUILD_IN_FUNCTIONS) - set(CONSTANTS)
     if undefined:
         raise ValueError(f"Parametros no definidos: {', '.join(undefined)}")
 
-    _shunting_yard = shunting_yard(tokens)
+    _shunting_yard = shunting_yard(tokens).tokens
 
     return name, params, _shunting_yard
 
-#2 tercero: Identifica los elementos del cuerpo por lo que son (numeros operaciones funciones etc)
+
 def pre_process_expression(expression):
     matches = re.finditer(
-        r'(?P<float>-?\d+\.\d+)|(?P<int>-?\d+)|(?P<function_call>\w+\()|(?P<LPAR>\()|(?P<RPAR>\))|(?P<ident>\w+)|(?P<symbol>[+\-*/^])|(?P<args_separator>,)|(?:\s*)',
+        PATTERN,
         expression
     )
 
     tokens = [
-        (token.lastgroup, token.group())
+        (TOKENS_TYPES(token.lastgroup), token.group())
         for token in matches
         if token.lastgroup is not None
     ]
+
+    tokens = [
+        (t_type, t_value[:-1])
+        if t_type == TOKENS_TYPES.FUNCTION_CALL
+        else (t_type, t_value)
+        for (t_type, t_value)
+        in tokens
+    ]
     return tokens
 
-#2 cuarto: Devuelve la forma arreglada del cuerpo de la funcion 
-def shunting_yard(tokens):
-    output, ops = deque(), []
-    arg_count_stack = []
 
-    for t_name, token in tokens:
-        if t_name in {'float', 'int'}:
-            output.append(float(token))
-        elif t_name == 'ident':
-            output.append(token)
-        elif t_name == 'function_call':
-            func_name = token[:-1]
-            if func_name in FUNCTIONS or func_name in func_defs:
-                ops.append(func_name)
-                arg_count_stack.append(0)
-                ops.append('(')
-            else:
-                print([*func_defs.keys()])
-                raise ValueError(f"Token desconocido: {func_name}")
-        elif token == ',':
-            while ops and ops[-1] != '(':
-                output.append(ops.pop())
-            if arg_count_stack:
-                arg_count_stack[-1] += 1
-        elif token in OPERATORS:
-            if (not output or output[-1] in OPERATORS or output[-1] in {'(', ')'}) and token == '-':
-                token = 'neg'
-            while (ops and ops[-1] in OPERATORS and
-                    ((OPERATORS[token][1] == 'L' and OPERATORS[token][0] <= OPERATORS[ops[-1]][0]) or
-                     (OPERATORS[token][1] == 'R' and OPERATORS[token][0] < OPERATORS[ops[-1]][0]))):
-                output.append(ops.pop())
-            ops.append(token)
-        elif token == '(':
-            ops.append(token)
-        elif token == ')':
-            while ops and ops[-1] != '(':
-                output.append(ops.pop())
-            ops.pop()
-            if ops and ops[-1] not in OPERATORS and ops[-1] not in {'(', ')'}:
-                func = ops.pop()
-                arg_count = arg_count_stack.pop() + 1
-                output.append((func, arg_count))
-
-    while ops:
-        output.append(ops.pop())
-
-    return output
-
-
-#3 primero: Trata de evaluar la exprecion y salta error si hay un problema
-def handle_evaluate(expression):
-    try:
-        result = full_evaluate_expression(expression, {})
-        return result
-    except (ValueError, ZeroDivisionError) as e:
-        return f"Error: {e}"
-
-#3 segundo: Ordena la eprecion a evaluar y la manda a calcular 
-def full_evaluate_expression(expression, vars):
+def evaluate_expression(expression, vars):
 
     tokens = pre_process_expression(expression)
-    _shunting_yard = shunting_yard(tokens)
+    _shunting_yard = shunting_yard(tokens).tokens
 
     return evaluate_postfix(_shunting_yard, vars)
-
 
 
 def log_interaction(log_file, interaction):
     with open(log_file, 'a') as f:
         f.write(interaction + '\n')
+
+
+_rango = [-3, 3, 0.01]
+
+
+def _handle_gen(args):
+    func_name, config_args = args[0]
+    args = [*args[1:]]
+    func_description = USERS_FUNCTIONS[func_name]
+    return [*args, evaluate_function(func_description, args)]
+
+
+def save_database():
+    print("Guardando base de datos")
+    print(f"Funciones definidas: {[*USERS_FUNCTIONS.keys()]}")
+
+    with open(f'.save_functions.txt', mode='wb') as f:
+        pickle.dump(USERS_FUNCTIONS, f)
+
+
+# region handlers ...
+
+def handle_define(func_def):
+
+    func_name, func_params, func_body = parse_function_definition(func_def)
+
+    try:
+        USERS_FUNCTIONS[func_name] = dict(
+            func_params=func_params,
+            func_body=func_body,
+            func_def=func_def
+        )
+        print(f"Función '{func_name}' definida exitosamente.")
+    except ValueError as e:
+        print(f"Error al definir la función '{func_name}': {e}")
+
+
+def handle_evaluate(expression):
+    try:
+        return evaluate_expression(expression, {})
+    except (ValueError, ZeroDivisionError) as e:
+        return f"Error: {e}"
 
 
 def handle_list(func_defs):
@@ -312,8 +116,83 @@ def handle_list(func_defs):
         return "Aun no hay funciones definidas."
 
 
-_rango = [-5, 5, 0.01]
+def handle_gen(function_name, *args):
+    save_database()
 
+    function_description = USERS_FUNCTIONS[function_name]
+    params_definition = function_description['func_params']
+    body = function_description['func_body']
+    func_def = function_description['func_def']
 
-def test__handle_gen():
-    _handle_gen('f', 1)
+    params = []
+    for _ in range(len(params_definition)):
+        params.append(
+            list(
+                np.arange(
+                    _rango[0],
+                    _rango[1],
+                    _rango[2]
+                )
+            )
+        )
+
+    result = []
+    with Pool(5) as p:
+        result = p.map(
+            _handle_gen,
+            product([(function_name, args)], *params)
+        )
+
+    np_result = np.array(result)
+
+    fig, ax = plt.subplots()
+
+    # Plot some data on the Axes.
+    if np_result.shape[1] == 2:
+        ax.plot(np_result[:, 0], np_result[:, 1])
+    elif np_result.shape[1] == 3:
+        data = np_result
+
+        # Extraer valores únicos de z
+        zs = np.unique(data[:, 1])
+
+        # Preparar la figura y los ejes
+        ax.set_xlim(np_result[:, 0].min(),
+                    np_result[:, 0].max())  # Límites para x
+        # Límites para y (considerando el desplazamiento máximo)
+        ax.set_ylim(np_result[:, 2].min(), np_result[:, 2].max())
+        line, = ax.plot([], [], 'b-', lw=3)  # Línea inicial vacía
+
+        # Título y etiquetas
+        ax.set_title(func_def)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+
+        def init():
+            """Inicializa la animación limpiando la línea."""
+            line.set_data([], [])
+            return line,
+
+        def update(z):
+            """Actualiza la figura para un valor de z dado."""
+            # Filtrar los datos para el z actual
+            filtered_data = data[data[:, 1] == z]
+            x = filtered_data[:, 0]
+            y = filtered_data[:, 2]
+            line.set_data(x, y)  # Establecer los nuevos datos de la línea
+            return line,
+
+        # Crear la animación
+        ani = FuncAnimation(
+            fig,
+            update,
+            frames=zs,
+            init_func=init,
+            blit=True,
+            # repeat=True,
+            interval=1000/30
+        )
+
+    plt.show()
+
+# endregion handlers ...
